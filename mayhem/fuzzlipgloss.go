9680825c615a5a53b1a66068ebe28698b7275b55
Brandon Watson <bmwtsn99@gmail.com>
2023-04-21 21:33:02 -0700

    add GitHub action to launch Mayhem

---
+++ mayhem/fuzzlipgloss.go
@@ -0,0 +1,79 @@
+package fuzzlipgloss
+
+import (
+    "strconv"
+    fuzz "github.com/AdaLogics/go-fuzz-headers"
+
+    "github.com/charmbracelet/lipgloss"
+)
+
+func mayhemit(data []byte) int {
+
+    var num int
+    if len(data) > 2 {
+        num, _ = strconv.Atoi(string(data[0]))
+        data = data[1:]
+        fuzzConsumer := fuzz.NewConsumer(data)
+        
+        switch num {
+            case 0:
+                testStringA, _ := fuzzConsumer.GetString()
+                testStringB, _ := fuzzConsumer.GetString()
+                testfloat, _ := fuzzConsumer.GetFloat64()
+                testPos := lipgloss.Position(testfloat)
+                
+                lipgloss.JoinHorizontal(testPos, testStringA, testStringB)
+                return 0
+
+            case 2:
+                testStringA, _ := fuzzConsumer.GetString()
+                testStringB, _ := fuzzConsumer.GetString()
+                testfloat, _ := fuzzConsumer.GetFloat64()
+                testPos := lipgloss.Position(testfloat)
+                
+                lipgloss.JoinVertical(testPos, testStringA, testStringB)
+                return 0
+
+            case 3:
+                testWidth, _ := fuzzConsumer.GetInt()
+                thestHeight, _ := fuzzConsumer.GetInt()
+                testfloat1, _ := fuzzConsumer.GetFloat64()
+                testfloat2, _ := fuzzConsumer.GetFloat64()
+                testString, _ := fuzzConsumer.GetString()
+                testhPos := lipgloss.Position(testfloat1)
+                testvPos := lipgloss.Position(testfloat2)
+
+                lipgloss.Place(testWidth, thestHeight, testhPos, testvPos, testString)
+                return 0
+
+            case 4:
+                testStyle := lipgloss.NewStyle()
+                testString1, _ := fuzzConsumer.GetString()
+                testString2, _ := fuzzConsumer.GetString()
+
+                testStyle.SetString(testString1, testString2)
+                return 0
+
+            case 5:
+                var testStyle lipgloss.Style
+                fuzzConsumer.GenerateStruct(&testStyle)
+                
+                testStyle.Copy()
+                return 0
+
+            case 6:
+                var testStyle lipgloss.Style
+                fuzzConsumer.GenerateStruct(&testStyle)
+
+                otherStyle := lipgloss.NewStyle()
+                otherStyle.Inherit(testStyle)
+                return 0
+        }
+    }
+    return 0
+}
+
+func Fuzz(data []byte) int {
+    _ = mayhemit(data)
+    return 0
+}
