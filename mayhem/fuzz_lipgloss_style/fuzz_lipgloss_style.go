618b1bdbb225a4fed2217645270d1570db90dff8
Brandon Watson <bmwtsn99@gmail.com>
2023-04-25 21:34:40 -0700

    updated to multiple mayhemfiles

--- mayhem/fuzz_lipgloss_style/fuzz_lipgloss_style.go
+++ mayhem/fuzz_lipgloss_style/fuzz_lipgloss_style.go
@@ -1,7 +1,6 @@
-package fuzzlipgloss
+package fuzz_lipgloss_style
 
 import (
-    "strconv"
     fuzz "github.com/AdaLogics/go-fuzz-headers"
 
     "github.com/charmbracelet/lipgloss"
@@ -9,44 +8,14 @@ import (
 
 func mayhemit(data []byte) int {
 
-    var num int
     if len(data) > 2 {
-        num, _ = strconv.Atoi(string(data[0]))
+        num := int(data[0])
         data = data[1:]
         fuzzConsumer := fuzz.NewConsumer(data)
         
         switch num {
+
             case 0:
-                testStringA, _ := fuzzConsumer.GetString()
-                testStringB, _ := fuzzConsumer.GetString()
-                testfloat, _ := fuzzConsumer.GetFloat64()
-                testPos := lipgloss.Position(testfloat)
-                
-                lipgloss.JoinHorizontal(testPos, testStringA, testStringB)
-                return 0
-
-            case 2:
-                testStringA, _ := fuzzConsumer.GetString()
-                testStringB, _ := fuzzConsumer.GetString()
-                testfloat, _ := fuzzConsumer.GetFloat64()
-                testPos := lipgloss.Position(testfloat)
-                
-                lipgloss.JoinVertical(testPos, testStringA, testStringB)
-                return 0
-
-            case 3:
-                testWidth, _ := fuzzConsumer.GetInt()
-                thestHeight, _ := fuzzConsumer.GetInt()
-                testfloat1, _ := fuzzConsumer.GetFloat64()
-                testfloat2, _ := fuzzConsumer.GetFloat64()
-                testString, _ := fuzzConsumer.GetString()
-                testhPos := lipgloss.Position(testfloat1)
-                testvPos := lipgloss.Position(testfloat2)
-
-                lipgloss.Place(testWidth, thestHeight, testhPos, testvPos, testString)
-                return 0
-
-            case 4:
                 testStyle := lipgloss.NewStyle()
                 testString1, _ := fuzzConsumer.GetString()
                 testString2, _ := fuzzConsumer.GetString()
@@ -54,14 +23,14 @@ func mayhemit(data []byte) int {
                 testStyle.SetString(testString1, testString2)
                 return 0
 
-            case 5:
+            case 1:
                 var testStyle lipgloss.Style
                 fuzzConsumer.GenerateStruct(&testStyle)
                 
                 testStyle.Copy()
                 return 0
 
-            case 6:
+            case 2:
                 var testStyle lipgloss.Style
                 fuzzConsumer.GenerateStruct(&testStyle)
 
@@ -69,7 +38,7 @@ func mayhemit(data []byte) int {
                 otherStyle.Inherit(testStyle)
                 return 0
 
-            case 7:
+            case 3:
                 testString, _ := fuzzConsumer.GetString()
 
                 lipgloss.Size(testString)
